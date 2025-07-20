584dcc5bbeeaa776007f14e8537f8ee1ff5844a0
SSworup77 <sworup.mail@gmail.com>
2025-07-20 18:04:55 +0000

    added rail routes and validations

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,11 @@ Rails.application.routes.draw do
   # get "/products/:id/edit", to: "products#edit"
   # patch "/products/:id", to: "products#update"
   # put "/products/:id", to: "products#update"
-
   # delete "/products/:id", to: "products#destroy"
   resources :products
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
   # Defines the root path route ("/")
   # root "posts#index"
   root "products#index"
